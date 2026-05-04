440c479d0ab2f58df1edbd9059f77b7aca0b4c07
giovannimazzi <gioelor@libero.it>
2026-05-04 17:10:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Master Boolean in Web Development [152] - Specializzazione PHP/Laravel - Assegnazione L03
+"
+    />
+    <meta name="author" content="Giovanni Mazzi" />
+    <title>EX - Password Generator</title>
+
+    <!-- Icona Progetto -->
+    <!-- <link rel="icon" href="./assets/img/..." /> -->
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔑</text></svg>"
+    />
+
+    <!-- Bootstrap Icons -->
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+    /> -->
+
+    <!-- Google Fonts -->
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://..."
+      rel="stylesheet"
+    /> -->
+
+    <!-- Bootstrap CSS -->
+     <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+
+    <!-- Reset Style -->
+    <!-- <link rel="stylesheet" href="css/reset_style.css" /> -->
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/style.css" />
+
+  </head>
+  <body>
+    <div class="container mt-5">
+      <h1 class="mb-2 text-center">Strong Password Generator</h1>
+      <h2 class="mb-1 text-center text-light">Genera una password sicura</h2>
+    </div>  
+  </body>
+</html>
